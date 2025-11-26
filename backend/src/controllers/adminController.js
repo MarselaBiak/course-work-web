c9ef7d8fdfaa21b9837179f314ad0db6d5423cc1
Marsela <marselabiyakiv@gmail.com>
2025-11-26 15:54:49 +0200

    Initial commit

--- backend/src/controllers/adminController.js
+++ backend/src/controllers/adminController.js
@@ -3,7 +3,7 @@ import Order from "../models/Order.js";
 
 export const getAllUsers = async (req, res) => {
     try {
-        const users = await User.find({}, "-password"); // без паролей
+        const users = await User.find({}, "-password"); 
         res.json(users);
     } catch (err) {
         res.status(500).json({ message: "Server error" });
